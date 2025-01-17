d0f8c500603cf413793a7871651d2deddec65516
Denis Makarenko <xhjkl@users.noreply.github.com>
2025-01-17 16:34:06 +0400

    make Vite understand path aliases

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,16 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@generated-types/araza.json': path.resolve(__dirname, '..', 'target', 'idl', 'araza.json'),
+			'@generated-types/araza': path.resolve(__dirname, '..', 'target', 'types', 'araza.ts'),
+		},
+	},
 	plugins: [
 		solid(),
 		nodePolyfills({
